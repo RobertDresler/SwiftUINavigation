5116a9e1d388f73ab8c30d58137e101a28b43dad
Robert Dresler <robertdreslerjr@gmail.com>
2025-01-21 18:08:26 +0100

    Reorganize project

---
+++ Examples/Modules/Start/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Start",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(
+            name: "Start",
+            targets: ["Start"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../../Navigation/ExamplesNavigation"),
+        .package(path: "../../Services/FlagsRepository"),
+        .package(path: "../SwiftUINavigation"),
+        .package(path: "../../Services/OnboardingService")
+    ],
+    targets: [
+        .target(
+            name: "Start",
+            dependencies: [
+                .product(name: "ExamplesNavigation", package: "ExamplesNavigation"),
+                .product(name: "FlagsRepository", package: "FlagsRepository"),
+                .product(name: "SwiftUINavigation", package: "SwiftUINavigation"),
+                .product(name: "OnboardingService", package: "OnboardingService")
+            ]
+        )
+    ]
+)
---
+++ Examples/Navigation/ExamplesNavigation/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ExamplesNavigation",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(
+            name: "ExamplesNavigation",
+            targets: ["ExamplesNavigation"]
+        )
+    ],
+    dependencies: [.package(path: "../SwiftUINavigation")],
+    targets: [
+        .target(
+            name: "ExamplesNavigation",
+            dependencies: [.product(name: "SwiftUINavigation", package: "SwiftUINavigation")]
+        )
+
+    ]
+)
